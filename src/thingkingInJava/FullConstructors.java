1be3485810e77f63e5aa52732bb2e7dfb736e408
Wincher <wincher.hoo@gmail.com>
2017-08-04 03:05:00 +0800

    thinking in java

---
+++ src/thingkingInJava/FullConstructors.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Wincher on 2017/6/26.
+ * src:12.4创建自定义异常
+ */
+public class FullConstructors {
+    public static void f() throws MyException {
+        System.out.println("Throwing MyException from f()...");
+        throw new MyException();
+    }
+    public static void g() throws MyException {
+        System.out.println("Throwing MyException from g()...");
+        throw new MyException("Originated in g()");
+    }
+    public static void main(String[] args) {
+        try {
+            f();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+        try {
+            g();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class MyException extends Exception {
+    public MyException() {}
+
+    public MyException(String message) { super(message); }
+}
---
+++ src/thingkingInJava/NestingInterfaces.java
@@ -0,0 +1,80 @@
+/**
+ * Created by Wincher on 2017/6/20.
+ * source: 9.8 嵌套接口
+ */
+class A{
+    interface B{
+        void f();
+    }
+    public class BImp implements B{
+        public void f(){}
+    }
+    private class BImp2 implements B{
+        public void f(){}
+    }
+    interface C{
+        void f();
+    }
+    class CImp implements C {
+        public void f(){}
+    }
+    public class CImp2 implements C {
+        public void f(){}
+    }
+    private interface D{
+        void f();
+    }
+    private class DImp implements D {
+        public void f(){}
+    }
+    public class DImp2 implements D {
+        public void f() {}
+    }
+    public D getD() { return new DImp2(); }
+    private D dRef;
+    public void ReveivdD(D d){
+        dRef = d;
+        dRef.f();
+    }
+}
+interface E {
+    interface G{
+        void f();
+    }
+    //redundant "public"
+    public interface H{
+        void f();
+    }
+    void g();
+
+}
+public class NestingInterfaces {
+    public class BImp implements A.B {
+        public void f(){}
+    }
+    class CImp implements A.C{
+        public void f(){}
+    }
+    //private Interface 不能被实现
+    //class Dimp implements A.D{public void f() {}}
+    class EImp implements E {
+        public void g() {}
+    }
+    class EGImp implements E.G{
+        public void f() {}
+    }
+    class EImp2 implements E{
+        public void g() {}
+        class EG implements G{
+            @Override
+            public void f() {}
+        }
+    }
+    public static void main(String[] args) {
+        A a = new A();
+        A a2 = new A();
+        a2.ReveivdD(a.getD());
+
+    }
+}
+
