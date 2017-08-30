018b57effd3aa3bd58659a509e82326fe88ef926
Wincher <wincher.hoo@gmail.com>
2017-08-30 22:25:15 +0800

    MultiThread

---
+++ src/Netty/cn/wincher/netty/quickStart/README.md
@@ -0,0 +1,16 @@
+quickstart
+从客户端发送一个字符串给服务器端处理
+服务器端处理完成返回结果给客户端
+
+multiHandler
+服务器端通过多个handler去处理一次请求数据
+
+传输object
+从客户端发送一个对象给服务端
+
+Netty能解决的问题
+Dubbo Hadoop spark 底层全部是用的netty
+
+1.首先弄一个bootstrap 对象
+2.封装bootstrap 对象
+3.
---
+++ src/Netty/cn/wincher/netty/quickStart/client/StrClient.java
@@ -0,0 +1,69 @@
+package Netty.cn.wincher.netty.quickStart.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by wincher on 30/08/2017.
+ */
+public class StrClient {
+	
+	private String host;
+	private int port;
+	private NioEventLoopGroup eventExecutors;
+	
+	public StrClient(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			new StrClient("localhost",8889).start();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void start() throws InterruptedException {
+		try {
+			//客户端的引导类，启动网络客户端
+			Bootstrap bootstrap = new Bootstrap();
+			//构造bootstrap对象
+			
+			// 可以理解成是一个线程池，用这个线程池来处理连接和接收数据
+			eventExecutors = new NioEventLoopGroup();
+			
+			bootstrap.group(eventExecutors) //多线程处理， 注册一下这个组
+					.channel(NioSocketChannel.class) //制定通道类型NioSocketChannel
+					.remoteAddress(new InetSocketAddress(host, port))  //注册远程服务器地址
+					.handler(new ChannelInitializer<SocketChannel>() {
+						//------------- ⬆️模版代码 ---------------------
+						@Override
+						protected void initChannel(SocketChannel socketChannel) throws Exception {
+							// 业务逻辑
+							socketChannel.pipeline().addLast(new StrClientHandler());
+						}
+						
+						//-------------- ⬇️模版代码-----------------------
+					});
+			
+			//开始连接服务器
+			ChannelFuture channelFuture = bootstrap.connect().sync(); //等到连接成功，否则一直阻塞线程
+			channelFuture.channel().closeFuture().sync(); //接收数据之后阻塞
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			eventExecutors.shutdownGracefully().sync();
+		}
+		
+	}
+}
