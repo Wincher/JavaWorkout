ac267927d5d3449d0ba693349b4e93dd1bf1106d
Wincher <wincher.hoo@gmail.com>
2017-09-11 17:39:00 +0800

    ActiveMQ hello world!

---
+++ src/NIOServer/NIOClient.java
@@ -0,0 +1,13 @@
+package NIOServer;
+
+/**
+ * Created by wincher on 01/09/2017.
+ */
+public class NIOClient {
+	public static void main(String[] args) {
+		int port = 8899;
+		String host = "127.0.0.1";
+		System.out.println("NIOClient -> connect to server at port:" + port);
+		new MsgClient(host, port).start();
+	}
+}
---
+++ src/NIOServer/NIOServer.java
@@ -0,0 +1,13 @@
+package NIOServer;
+
+/**
+ * Created by wincher on 01/09/2017.
+ */
+public class NIOServer {
+	public static void main(String[] args) {
+		int port = 8899;
+		String host = "127.0.0.1";
+		//因为是nio，所以不需要线程干预
+		new NIOMsgServer(host, port).start();
+	}
+}
