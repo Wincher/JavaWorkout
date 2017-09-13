9f039d6b55a0402736f7ac1a94273a740fe20098
Wincher <wincher.hoo@gmail.com>
2017-09-13 15:19:50 +0800

    ActiveMQ session producer consumer features!

--- src/ActiveMQ_JMS/cn/wincher/activemq/helloworld/Receiver.java
+++ src/ActiveMQ_JMS/cn/wincher/activemq/helloworld/Receiver.java
@@ -8,14 +8,15 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by wincher on 11/09/2017.
  */
-public class Reveiver {
+public class Receiver {
 	
 	public static void main(String[] args) throws Exception {
 		//第一步：建立ConnectionFactory工厂对象，需要填入用户名，密码，以及要连接的地址，
 		// 均使用默认即可，默认端口为"tcp://127.0.0.1:61616"
 		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
-				ActiveMQConnectionFactory.DEFAULT_USER,
-				ActiveMQConnectionFactory.DEFAULT_PASSWORD,
+				//ActiveMQConnectionFactory.DEFAULT_USER,
+				//ActiveMQConnectionFactory.DEFAULT_PASSWORD,
+				"wincher","123456",
 				"tcp://127.0.0.1:61616");
 		
 		//第二步：通过ConnectionFactory工厂对象创建一个Connection连接，
@@ -25,7 +26,7 @@ public class Reveiver {
 		
 		//第三步：通过Connection对象创建Session会话（上下文环境对象，用于接收消息，
 		// 参数配置1为是否启用事务，参数配置2为签收模式，一般我们设置自动签收
-		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+		Session session = connection.createSession(Boolean.FALSE, Session.CLIENT_ACKNOWLEDGE);
 		
 		//第四步：通过Session创建Destination对象，指的是一个客户端用来指定生产消息目标的消费信息来源的对象，
 		// 在PTP模式中，Destination被称作Queue即队列：在Publisher
--- src/ActiveMQ_JMS/cn/wincher/activemq/session/Sender.java
+++ src/ActiveMQ_JMS/cn/wincher/activemq/session/Sender.java
@@ -1,4 +1,4 @@
-package ActiveMQ_JMS.cn.wincher.activemq.helloworld;
+package ActiveMQ_JMS.cn.wincher.activemq.session;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Created by wincher on 11/09/2017.
+ * 如果session开启事务，session需要手动commit才能提交
  */
 public class Sender {
 	public static void main(String[] args) throws Exception {
@@ -25,7 +26,8 @@ public class Sender {
 		
 		//第三步：通过Connection对象创建Session会话（上下文环境对象，用于接收消息，
 		// 参数配置1为是否启用事务，参数配置2为签收模式，一般我们设置自动签收
-		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+		//consumer的策略会以publisher为准，为了可读性建议consumer的session初始化与publisher一致
+		Session session = connection.createSession(Boolean.TRUE, Session.CLIENT_ACKNOWLEDGE);
 		
 		//第四步：通过Session创建Destination对象，指的是一个客户端用来指定生产消息目标的消费信息来源的对象，
 		// 在PTP模式中，Destination被称作Queue即队列：在Publisher
@@ -35,14 +37,13 @@ public class Sender {
 		//参数为此生产者发送数据的目的地，可以在发送的时候再次指定更加灵活所以此处设置为null
 		MessageProducer producer = session.createProducer(null);
 		
-		//第六步：我们可以使MessageProducer的setDeliveryMode方法为其设置持久化特性和非持久化特性(DeliveryMode)，***
-		// NON_PERSISTENT重启丢数据，不入库，
-		producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+		//第六步：我们可以使MessageProducer的setDeliveryMode方法为其设置持久化特性和非持久化特性(DeliveryMode)，****
+		//producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
 		
 		//第七步：使用JMS规范的TextMessage形式创建数据（通过Session对象），并用MessageProducer的send方法发送数据，
 		//同理客户端使用receive方法进行接收
 		
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 10; i++) {
 			TextMessage msg = session.createTextMessage("Message content " + i);
 			//msg.setText("这个方法也可以");;
 			//第一个参数 目标地址
@@ -52,8 +53,9 @@ public class Sender {
 			//第五个参数 消息的过期时间
 			producer.send(destination, msg);
 			System.out.println(msg.getText());
-			TimeUnit.SECONDS.sleep(1);
 		}
+		
+		session.commit();
 		
 //		for (int i = 0; i < 5; i++) {
 //			MapMessage map = session.createMapMessage();
