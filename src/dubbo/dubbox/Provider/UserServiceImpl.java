0e68434f6b120c638f28f2890d417dec3af81904
Wincher <wincher.hoo@gmail.com>
2017-10-25 01:54:59 +0800

    update

--- src/dubbo/dubbox/Provider/UserServiceImpl.java
+++ src/dubbo/dubbox/Provider/UserServiceImpl.java
@@ -14,22 +14,14 @@ import javax.ws.rs.core.MediaType;
  * Created by wincher on 24/10/2017.
  */
 
-//要添加tomcat支持的jar包，jboss-logging.jar
-@Path("/userService")
-@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
-@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
 @Service("userService")
 @com.alibaba.dubbo.config.annotation.Service(interfaceClass = UserServcie.class,protocol = {"rest","dubbo"},retries = 0)
 public class UserServiceImpl implements UserServcie {
-	@GET
-	@Path("/testGet")
 	@Override
 	public void testGet() {
 		System.out.println("test get...");
 	}
 	
-	@GET
-	@Path("/getUser")
 	@Override
 	public User getUser() {
 		User u = new User("123", "jonny", 19, "m");
