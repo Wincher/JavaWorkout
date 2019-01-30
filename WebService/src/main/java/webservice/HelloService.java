ed48e65a28d6517d838923949ea75f69b691b2b3
wincher <wincher.hoo@gmail.com>
2019-01-30 23:48:47 +0800

    feature: webservice

---
+++ WebService/src/main/java/webservice/HelloService.java
@@ -0,0 +1,25 @@
+package webservice;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.ws.Endpoint;
+
+/**
+ * @author wincher
+ * @since 2018/10/18
+ * <p> webservice <p>
+ */
+//@SOAPBinding(style = SOAPBinding.Style.RPC)
+@WebService
+public class HelloService {
+
+  @WebMethod
+  public String sayHi(String name) {
+    return "Hi, " + name;
+  }
+  
+  public static void main(String[] args) {
+    Endpoint endpoint = Endpoint.publish("http://localhost:8080/hiws", new HelloService());
+  }
+}
