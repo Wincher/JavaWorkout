27291daea4f257e10d4194c4cddac2dea0c1d45d
wincher <wincher.hoo@gmail.com>
2021-02-20 17:44:14 +0800

    update: ConcurrentHashMap

--- JAVASE/src/main/java/concurrentMap/TestConcurrentHashMap.java
+++ JAVASE/src/main/java/concurrentMap/TestConcurrentHashMap.java
@@ -1,5 +1,7 @@
 package concurrentMap;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ForkJoinPool;
@@ -25,17 +27,25 @@ public class TestConcurrentHashMap {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        //其实道理显而易见,根本没必要这样来实验, 只要知道ConcurrentHashMap 只能保证原子性的读写操作是线程安全的,
-        //并不能保证多操作的线程安全, 需要手动同步保证, 但是提供了原子性方法 putIfAbsent 等做复合逻辑
-        //那么 ConcurrentHashMap存在的意义在哪儿呢,我们都知道是线程安全的Map, 怎么就不安全了呢
-        //最主要的是避免了在并发场景下HashMap的回环链表问题
+        //可以看到
+        HashMap<Integer, Integer> map = new HashMap<>();
+        testIt(map);
+        ConcurrentHashMap<Integer, Integer> map1 = new ConcurrentHashMap<>();
+        testIt(map1);
+        //这里多做几次实验就会发现会出现不一致的情况, 这就是HashMap非线程安全的问题所在了, 并发条件下Hash冲突可能丢失元素, 还可能size与实际size不一致
+        System.out.println(map.size() + ":" + map.keySet().stream().count());
+//        其实道理显而易见,根本没必要这样来实验, 只要知道ConcurrentHashMap 只能保证原子性的读写操作是线程安全的,
+//        并不能保证多操作的线程安全, 需要手动同步保证, 但是提供了原子性方法 putIfAbsent 等做复合逻辑
+//        那么 ConcurrentHashMap存在的意义在哪儿呢,我们都知道是线程安全的Map, 怎么就不安全了呢
+//        其中之一是避免了在并发场景下HashMap的回环链表问题
+//        回环链表问题是因为并发的resize, 并且由于链表尾遍历, 导致 B -> A -> B的问题出现死循环的问题, JDK1.8后已经增加了tail指针避免了死循环问题
         ConcurrentHashMap<String, Long> concurrentHashMap = getData(ITEM_COUNT - 90000);
         System.out.println("init size: " + concurrentHashMap.size());
         ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
         forkJoinPool.execute(() -> IntStream.rangeClosed(1, 10).parallel().forEach( i -> {
             int gap = ITEM_COUNT - concurrentHashMap.size();
             while (0 < gap) {
-                //去掉同步代码块和dobule check就回出现线程安全问题, 导致最后并不能准确的将concurrentHashMap填满到ITEM_COUNT, 而是会溢出
+                //double check就回出现线程安全问题, 导致最后并不能准确的将concurrentHashMap填满到ITEM_COUNT, 而是会溢出
                 synchronized (concurrentHashMap) {
                     gap = ITEM_COUNT - concurrentHashMap.size();
                     if (0 < gap) {
@@ -49,4 +59,10 @@ public class TestConcurrentHashMap {
         forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
         System.out.println("finish size: " + concurrentHashMap.size());
     }
+
+    public static void testIt(Map<Integer, Integer> map) throws InterruptedException {
+        IntStream.range(0, 100).parallel().forEach(i -> map.put(i, -1));
+        System.out.println(map.size());
+    }
+
 }
